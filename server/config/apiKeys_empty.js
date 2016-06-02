38a5c709bbf5c4984d0ae3c73030d5b2d09b898c
Greg Smith <crashingwaves@fea.st>
2016-06-02 12:41:58 -0600

    Updated twitchHelper to use clientID per new twitch reqs.

--- server/config/apiKeys_empty.js
+++ server/config/apiKeys_empty.js
@@ -4,5 +4,7 @@
 module.exports = {
   weatherKey: "",
   mapsKey: "",
-  twitchKey: ""
+  twitchClientId: "",
+  twitchSecret: "",
+  flickr: ""
 };
--- server/helpers/twitchHelper.js
+++ server/helpers/twitchHelper.js
@@ -1,49 +1,56 @@
 'use strict';
 
-const fetchDataFrom = require('./fetchDataFrom');
+const fetchDataFrom = require('./fetchDataFrom'),
+      apiKeys = require('../config/apiKeys');
 
 module.exports = (request, response) => {
 
-  const userName = request.params.userName,
+    const userName = request.params.userName,
         streamObj = {
-          protocol: 'https:',
-          hostname: 'api.twitch.tv',
-          path: `/kraken/streams/${userName}`
+            protocol: 'https:',
+            hostname: 'api.twitch.tv',
+            path: `/kraken/streams/${userName}`,
+            headers: {
+              'Client-ID': apiKeys.twitchClientId
+            }
         },
         channelObj = {
-          protocol: 'https:',
-          hostname: 'api.twitch.tv',
-          path: `/kraken/channels/${userName}`
+            protocol: 'https:',
+            hostname: 'api.twitch.tv',
+            path: `/kraken/channels/${userName}`,
+            headers: {
+              'Client-ID': apiKeys.twitchClientId
+            }
         };
-  Promise.all([fetchDataFrom(streamObj),fetchDataFrom(channelObj)])
+    Promise.all([fetchDataFrom(streamObj), fetchDataFrom(channelObj)])
         .then((fetchedData) => {
-          let streamData = fetchedData[0].stream,
-              channelData = fetchedData[1];
+            let streamData = fetchedData[0].stream,
+                channelData = fetchedData[1];
 
-          let twitchObj = {
-            isStreaming: streamData ? true : false,
-            game: streamData ? streamData.game : null,
-            preview: streamData ? streamData.preview.large : null,
-            status: channelData.status,
-            display_name: channelData.display_name,
-            logo: channelData.logo || "http://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_150x150.png",
-            url: channelData.url
-          };
-          response.send(twitchObj);
+            let twitchObj = {
+                isStreaming: streamData ? true : false,
+                game: streamData ? streamData.game : null,
+                preview: streamData ? streamData.preview.large : null,
+                status: channelData.status,
+                display_name: channelData.display_name,
+                logo: channelData.logo || "http://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_150x150.png",
+                url: channelData.url
+            };
+            response.send(twitchObj);
         })
         .catch((error) => {
-          if(error.statusCode === 404) {
-            response.send({
-              isStreaming: false,
-              game: null,
-              preview: null,
-              status: "User does not exist",
-              display_name: userName,
-              logo: "http://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_150x150.png",
-              url: null
-            })
-          } else {
-            response.send(error);
-          }
+            if (error.statusCode === 404) {
+                response.send({
+                    isStreaming: false,
+                    game: null,
+                    preview: null,
+                    status: "User does not exist",
+                    display_name: userName,
+                    logo: "http://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_150x150.png",
+                    url: null
+                })
+            } else {
+                response.send(error);
+            }
         })
 };
