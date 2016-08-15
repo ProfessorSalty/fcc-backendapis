af6e092aa12997a4c2e2315cfec873e756f7c0a5
Greg Smith <crashingwaves@fea.st>
2016-08-15 15:18:34 -0600

    Setup logging

--- server/routes/mailer.js
+++ server/routes/mailer.js
@@ -1,6 +1,24 @@
 const router = require('express').Router(),
     nodemailer = require('nodemailer'),
-    transporter = nodemailer.createTransport('smtps://warbread%40gmail.com:uficbvfxdanbjldp@smtp.gmail.com');
+    transporter = nodemailer.createTransport('smtps://warbread%40gmail.com:uficbvfxdanbjldp@smtp.gmail.com'),
+    winston = require('winston'),
+    path = require('path'),
+    logPath = path.resolve(__dirname, '..', 'logs'),
+    logger = new (winston.Logger)({
+        transports: [
+            new (winston.transports.Console)(),
+            new (winston.transports.File)({
+                name: 'error-file',
+                filename: logPath + '/error-file.winston_log',
+                level: 'error'
+            }),
+            new (winston.transports.File)({
+                name: 'debug-file',
+                filename: logPath + '/debug-file.winston_log',
+                level: 'debug'
+            })
+        ]
+    });
 
 router.post('/', (request, response) => {
     const message = `
@@ -8,21 +26,26 @@ router.post('/', (request, response) => {
       <h2><b>From:</b> ${request.body.from}</h2>
       <p>${request.body.text}</p>
     `
+    logger.info('New message coming in:', request.body);
+
     mailOptions = {
         from: request.body.from,
         to: 'crashingwaves@fea.st',
         subject: 'Someone is trying to contact you!',
         html: message
     };
+
+    logger.debug('mailOptions content:', mailOptions);
+
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             response.status(500);
             response.send(error);
-            return console.log(error);
+            return logger.error(error);
         }
         response.status(200);
         response.send('message sent');
-        console.log('Message sent: ' + info.response);
+        logger.info('Message sent: ' + info.response);
     });
 });
 
