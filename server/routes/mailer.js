b16f516f5bdeb82f89b5cf203d40590883bc055d
Greg Smith <crashingwaves@fea.st>
2016-08-15 11:09:27 -0600

    Setup nodemailer

--- server/routes/mailer.js
+++ server/routes/mailer.js
@@ -1,1 +1,22 @@
-const nodemailer = require('nodemailer');
+const router = require('express').Router(),
+    nodemailer = require('nodemailer'),
+    transporter = nodemailer.createTransport('smtps://warbread%40gmail.com:uficbvfxdanbjldp@smtp.gmail.com'),
+    mailOptions = {
+        from: 'Website Admin',
+        to: 'crashingwaves@fea.st',
+        subject: 'Web contact',
+        text: 'Someone is contacting you!'
+    };
+
+router.post('/', (request, response) => {
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            return console.log(error);
+        }
+        response.status(200);
+        response.send('message sent');
+        console.log('Message sent: ' + info.response);
+    });
+});
+
+module.exports = router;
