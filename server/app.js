29bc7eee47a640355bca6a18797df63d79a29108
Greg Smith <crashingwaves@fea.st>
2016-08-15 15:18:15 -0600

    Create log directory if none found

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-var express = require('express'),
+const express = require('express'),
     app = express(),
     config = require('./config/config'),
     port = +process.argv[2] || config.devPort,
@@ -7,7 +7,6 @@ var express = require('express'),
     shortener = require('./routes/urlshortener'),
     bodyParser = require('body-parser'),
     expressHandlbars = require('express-handlebars'),
-    viewsPath = __dirname + '/views',
     handlebarsHelpers = require('./helpers/handlebarsHelpers'),
     weatherFetcher = require('./routes/weatherFetcher'),
     twitchFetcher = require('./routes/twitchFetcher'),
@@ -15,7 +14,15 @@ var express = require('express'),
     quoteFetcher = require('./routes/quoteFetcher'),
     imageSearch = require('./routes/imageSearch'),
     fileMetadataViewer = require('./routes/fileMetadataViewer'),
-    mailer = require('./routes/mailer.js');
+    mailer = require('./routes/mailer.js'),
+    viewsPath = __dirname + '/views',
+    logDir = __dirname + '/logs';
+
+try {
+    require('fs').statSync(logDir);
+} catch ( e ) {
+    require('fs').mkdirSync(logDir);
+}
 
 app.engine('handlebars', expressHandlbars({
     defaultLayout: 'main',
