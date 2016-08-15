d448fd3d6a9344350be36e11592a6626d6618916
Greg Smith <crashingwaves@fea.st>
2016-08-15 10:53:04 -0600

    Add nodemailer

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 var express = require('express'),
-    app     = express(),
-    config  = require('./config/config'),
-    port    = +process.argv[2] || config.devPort,
-    time    = require('./routes/timestamp'),
+    app = express(),
+    config = require('./config/config'),
+    port = +process.argv[2] || config.devPort,
+    time = require('./routes/timestamp'),
     headerParser = require('./routes/headerparser'),
     shortener = require('./routes/urlshortener'),
     bodyParser = require('body-parser'),
@@ -14,46 +14,56 @@ var express = require('express'),
     wikipediaViewer = require('./routes/wikipediaViewer'),
     quoteFetcher = require('./routes/quoteFetcher'),
     imageSearch = require('./routes/imageSearch'),
-    fileMetadataViewer = require('./routes/fileMetadataViewer');
+    fileMetadataViewer = require('./routes/fileMetadataViewer'),
+    mailer = require('./routes/mailer.js');
 
-    app.engine('handlebars', expressHandlbars({defaultLayout: 'main', layoutsDir: viewsPath + '/layouts', helpers: handlebarsHelpers}));
-    app.set('view engine', 'handlebars');
-    app.set('views', viewsPath);
+app.engine('handlebars', expressHandlbars({
+    defaultLayout: 'main',
+    layoutsDir: viewsPath + '/layouts',
+    helpers: handlebarsHelpers
+}));
+app.set('view engine', 'handlebars');
+app.set('views', viewsPath);
 
-    app.use((request, response, next) => {
-      response.header("Access-Control-Allow-Origin", "*");
-      response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      next();
-    });
+app.use((request, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-    app.get('/', (request, response) => {
-       response.render('index');
-    });
+app.get('/', (request, response) => {
+    response.render('index');
+});
 
-    app.use('/time', time);
+app.use('/time', time);
 
-    app.use('/header', headerParser);
+app.use('/header', headerParser);
 
-    app.use('/short', shortener);
+app.use('/short', shortener);
 
-    app.use('/weather', weatherFetcher);
+app.use('/weather', weatherFetcher);
 
-    app.use('/twitch', twitchFetcher);
+app.use('/twitch', twitchFetcher);
 
-    app.use('/wikiview', wikipediaViewer);
+app.use('/wikiview', wikipediaViewer);
 
-    app.use('/quote', quoteFetcher);
+app.use('/quote', quoteFetcher);
 
-    app.use('/image', imageSearch);
+app.use('/image', imageSearch);
 
-    app.use('/file', fileMetadataViewer);
+app.use('/file', fileMetadataViewer);
 
-    app.listen(port,  () => {
-        process.stdout.write(`Server listening on port ${port}\n`);
-    });
+app.use('/sendmail', mailer);
+
+app.listen(port, () => {
+    process.stdout.write(`Server listening on port ${port}
+`);
+});
 
 
 
