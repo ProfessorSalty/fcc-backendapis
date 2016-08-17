d31994a0a7442014b25f044c9796b4b64997345a
Greg Smith <crashingwaves@fea.st>
2016-08-16 22:05:37 -0600

    Change formatting

--- server/helpers/fetchDataFrom.js
+++ server/helpers/fetchDataFrom.js
@@ -1,29 +1,29 @@
 'use strict';
 
 module.exports = (requestObj) => {
-  return new Promise((resolve, reject) => {
-    const protocol = requestObj.protocol === 'https:' ? require('https') : require('http');
-    protocol.get(requestObj, (response) => {
-      if(response.statusCode < 200 || response.statusCode > 299) {
-        reject({
-          message:`Failed to get resource`,
-          statusCode: response.statusCode
+    return new Promise((resolve, reject) => {
+        const protocol = requestObj.protocol === 'https:' ? require('https') : require('http');
+        protocol.get(requestObj, (response) => {
+            if (response.statusCode < 200 || response.statusCode > 299) {
+                reject({
+                    message: `Failed to get resource for ${requestObj.hostname}`,
+                    statusCode: response.statusCode
+                });
+            }
+
+            let body = '';
+            response.on('data', (chunk) => {
+                body += chunk;
+            });
+
+            response.on('end', () => {
+                resolve(JSON.parse(body));
+            });
+
+            response.on('error', (error) => {
+                reject(error)
+            });
         });
-      }
-
-      let body = '';
-      response.on('data', (chunk) => {
-        body += chunk;
-      });
-
-      response.on('end', () => {
-        resolve(JSON.parse(body));
-      });
-
-      response.on('error', (error) => {
-        reject(error)
-      });
     });
-  });
 };
 
