3bd00c46f57cfc2c00839d6ff07e2da7808e9866
Greg Smith <crashingwaves@fea.st>
2016-08-16 22:05:59 -0600

    Fix typo

--- server/helpers/weatherHelper.js
+++ server/helpers/weatherHelper.js
@@ -19,58 +19,58 @@ function getNewDate(epoch) {
 
 module.exports = function weatherHelper(request, response) {
 
-    const currentPosition = request.params.currentPosition,
-        weatherObj = {
-            protocol: 'https:',
-            hostname: 'api.forecast.io',
-            path: '/forecast/' + apiKeys.weatherKey + '/' + currentPosition
-        },
-        mapsObj = {
-            protocol: 'https:',
-            hostname: 'maps.googleapis.com',
-            path: `maps/api/geocode/json?latlng=${currentPosition}&key=${apiKeys.mapsKey}`
-        }
+const currentPosition = request.params.currentPosition,
+    weatherObj = {
+        protocol: 'https:',
+        hostname: 'api.forecast.io',
+        path: `/forecast/${apiKeys.weatherKey}/${currentPosition}`
+    },
+    mapsObj = {
+        protocol: 'https:',
+        hostname: 'maps.googleapis.com',
+        path: `/maps/api/geocode/json?latlng=${currentPosition}&key=${apiKeys.mapsKey}`
+}
 
-    Promise.all([fetchDataFrom(weatherObj), fetchDataFrom(mapsObj)])
-        .then((data) => {
-            const weatherData = data[0],
-                cityData = data[1],
-                city = cityData.results[1].formatted_address;
-            let currentWeather = weatherData.currently,
-                nextWeekWeather = weatherData.daily.data;
+Promise.all([fetchDataFrom(weatherObj), fetchDataFrom(mapsObj)])
+    .then((data) => {
+        const weatherData = data[0],
+            cityData = data[1],
+            city = cityData.results[1].formatted_address;
+        let currentWeather = weatherData.currently,
+            nextWeekWeather = weatherData.daily.data;
 
-            currentWeather = {
-                time: getNewDate(currentWeather.time),
-                summary: currentWeather.summary,
-                icon: currentWeather.icon,
-                precipProbability: currentWeather.precipProbability,
-                temperature: currentWeather.temperature,
-                windSpeed: currentWeather.windSpeed,
-                windBearing: currentWeather.windBearing
+        currentWeather = {
+            time: getNewDate(currentWeather.time),
+            summary: currentWeather.summary,
+            icon: currentWeather.icon,
+            precipProbability: currentWeather.precipProbability,
+            temperature: currentWeather.temperature,
+            windSpeed: currentWeather.windSpeed,
+            windBearing: currentWeather.windBearing
+        };
+
+        nextWeekWeather = nextWeekWeather.map((x) => {
+            return {
+                time: getNewDate(x.time),
+                summary: x.summary,
+                icon: x.icon,
+                precipProbability: x.precipProbability,
+                temperature: x.temperature,
+                windSpeed: x.windSpeed,
+                windBearing: x.windBearing,
+                sunriseTime: getNewDate(x.sunriseTime),
+                sunsetTime: getNewDate(x.sunsetTime)
             };
-
-            nextWeekWeather = nextWeekWeather.map((x) => {
-                return {
-                    time: getNewDate(x.time),
-                    summary: x.summary,
-                    icon: x.icon,
-                    precipProbability: x.precipProbability,
-                    temperature: x.temperature,
-                    windSpeed: x.windSpeed,
-                    windBearing: x.windBearing,
-                    sunriseTime: getNewDate(x.sunriseTime),
-                    sunsetTime: getNewDate(x.sunsetTime)
-                };
-            });
-
-            response.send({
-                city: city,
-                currentWeather: currentWeather,
-                nextWeekWeather: nextWeekWeather
-            });
-
-        })
-        .catch((error) => {
-            response.send(error);
         });
+
+        response.json({
+            city: city,
+            currentWeather: currentWeather,
+            nextWeekWeather: nextWeekWeather
+        });
+
+    })
+    .catch((error) => {
+        response.json(error);
+    });
 };
