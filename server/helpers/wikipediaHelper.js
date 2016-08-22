4eaab0309f7f316084131c6d37e146d1d8650780
Greg Smith <crashingwaves@fea.st>
2016-08-21 22:10:59 -0600

    Parse request.body for data instead of query params

--- server/helpers/wikipediaHelper.js
+++ server/helpers/wikipediaHelper.js
@@ -2,39 +2,39 @@
 const fetchDataFrom = require("./fetchDataFrom");
 
 module.exports.search = (request, response) => {
-  const searchTerm = request.params.searchTerm,
+    const searchTerm = request.body.searchTerm,
         requestObj = {
-          protocol: 'https:',
-          hostname: 'en.wikipedia.org',
-          path: `/w/api.php?action=query&list=search&srsearch=${searchTerm}&utf8&format=json`
+            protocol: 'https:',
+            hostname: 'en.wikipedia.org',
+            path: `/w/api.php?action=query&list=search&srsearch=${searchTerm}&utf8&format=json`
         };
-  fetchDataFrom(requestObj)
-    .then((results) => {
-      const filteredResults = results.query.search.map( x => {
-        return {
-          title: x.title,
-          snippet: x.snippet,
-          url: 'https://en.wikipedia.org/wiki/' + x.title.replace(/\s/g, '_')
-        }
-      });
-      response.send(filteredResults);
-    })
-    .catch((error) => {
-      response.send(error);
-    })
+    fetchDataFrom(requestObj)
+        .then((results) => {
+            const filteredResults = results.query.search.map(x => {
+                return {
+                    title: x.title,
+                    snippet: x.snippet,
+                    url: 'https://en.wikipedia.org/wiki/' + x.title.replace(/\s/g, '_')
+                }
+            });
+            response.send(filteredResults);
+        })
+        .catch((error) => {
+            response.send(error);
+        })
 };
 
 module.exports.random = (request, response) => {
-  const randomRequestObject = {
-    protocol: 'https:',
-    hostname: 'en.wikipedia.org',
-    path: '/w/api.php?action=query&list=random&format=json&rnnamespace=0'
-  }
-  fetchDataFrom(randomRequestObject)
-    .then((result) => {
-      response.redirect(`https://en.wikipedia.org/wiki/${result.query.random[0].title}`);
-    })
-    .catch((error) => {
-      response.send(error);
-    })
+    const randomRequestObject = {
+        protocol: 'https:',
+        hostname: 'en.wikipedia.org',
+        path: '/w/api.php?action=query&list=random&format=json&rnnamespace=0'
+    }
+    fetchDataFrom(randomRequestObject)
+        .then((result) => {
+            response.redirect(`https://en.wikipedia.org/wiki/${result.query.random[0].title}`);
+        })
+        .catch((error) => {
+            response.send(error);
+        })
 };
