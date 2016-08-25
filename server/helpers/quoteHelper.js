2a7a4af023ce53f0322ac936e707859243a4bd00
Gregory Smith <crashingwaves@fea.st>
2016-08-25 13:50:39 -0600

    Switch to stands4 quotes API

--- server/helpers/quoteHelper.js
+++ server/helpers/quoteHelper.js
@@ -1,32 +1,34 @@
 'use strict';
-const fetchDataFrom = require('./fetchDataFrom');
+const fetchDataFrom = require('./fetchDataFrom'),
+    quoteKey = require('../config/apiKeys.js').quotes;
 
 module.exports = (request, response) => {
   //Get quotes from server
   const requestObj = {
                       protocol: 'http:',
-                      hostname: 'www.quotesondesign.com',
-                      path: '/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+                      hostname: 'www.stands4.com',
+                      path: `/services/v2/quotes.php?uid=${quoteKey.uid}&tokenid=${quoteKey.key}&searchtype=RANDOM&format=json`
                     };
   fetchDataFrom(requestObj)
     .then(result => {
-      let quote = result[0];
+      let quote = {
+        text: result.results.result[0].quote[0],
+        author: result.results.result[0].author[0]
+      };
       //Get wikipedia page based on the quote author's name
       const wikiRequest = {
         protocol: 'https:',
         hostname: 'en.wikipedia.org',
-        path: `/w/api.php?action=query&list=search&srsearch=${encodeURI(quote.title)}&utf8&format=json`
+        path: `/w/api.php?action=query&list=search&srsearch=${encodeURI(quote.author)}&utf8&format=json`
       }
       return fetchDataFrom(wikiRequest)
         .then(queries => {
           response.send({
-            quote: quote.content.replace(/(?:\<\/?\w*\>)|[\n\r]+/g, "").trim(),
-            author: quote.title,
+            quote: quote.text.replace(/(?:\<\/?\w*\>)|[\n\r]+/g, "").trim(),
+            author: quote.author,
             description: queries.query.search[0].snippet,
-            wikipediaLink: `https://en.wikipedia.org/wiki/${quote.title.replace(/\s/g, '_')}`
+            wikipediaLink: `https://en.wikipedia.org/wiki/${quote.author.replace(/\s/g, '_')}`
           });
-        }).catch(error => {
-          resonse.send(error);
         });
     })
     .catch(error => {
