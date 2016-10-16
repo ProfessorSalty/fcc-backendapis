fcf51d8cfbeef299e833275c030c5ada15b6f461
Gregory Smith <crashingwaves@fea.st>
2016-10-16 08:25:38 -0600

    Serve quotes locally

--- server/helpers/quoteHelper.js
+++ server/helpers/quoteHelper.js
@@ -1,37 +1,31 @@
 'use strict';
 const fetchDataFrom = require('./fetchDataFrom'),
-    quoteKey = require('../config/apiKeys.js').quotes;
+    quoteKey = require('../config/apiKeys.js').quotes,
+    quotes = require('../text/quotes.json').quotes;
 
 module.exports = (request, response) => {
-  //Get quotes from server
-  const requestObj = {
-                      protocol: 'http:',
-                      hostname: 'www.stands4.com',
-                      path: `/services/v2/quotes.php?uid=${quoteKey.uid}&tokenid=${quoteKey.key}&searchtype=RANDOM&format=json`
-                    };
-  fetchDataFrom(requestObj)
-    .then(result => {
-      let quote = {
-        text: result.results.result[0].quote[0] || result.results.result[0].quote || "No text found on server",
-        author: result.results.result[0].author[0]
-      };
-      //Get wikipedia page based on the quote author's name
-      const wikiRequest = {
+  const randomNumber = Math.floor(Math.random() * (quotes.length + 1)),
+        quote = quotes[randomNumber][0],
+        author = quotes[randomNumber][1];
+
+//Get wikipedia page based on the quote author's name
+  const wikiRequest = {
         protocol: 'https:',
         hostname: 'en.wikipedia.org',
-        path: `/w/api.php?action=query&list=search&srsearch=${encodeURI(quote.author)}&utf8&format=json`
+        path: `/w/api.php?action=query&list=search&srsearch=${author.replace(/\s/g, '+')}&utf8&format=json`
       }
-      return fetchDataFrom(wikiRequest)
+
+  fetchDataFrom(wikiRequest)
         .then(queries => {
           response.send({
-            quote: quote.text.replace(/(?:\<\/?\w*\>)|[\n\r]+/g, "").trim(),
-            author: quote.author,
+            quote: quote,
+            author: author,
             description: queries.query.search[0].snippet,
-            wikipediaLink: `https://en.wikipedia.org/wiki/${quote.author.replace(/\s/g, '_')}`
+            wikipediaLink: `https://en.wikipedia.org/wiki/${author.replace(/\s/g, '_')}`
           });
-        });
-    })
+        })
     .catch(error => {
-      response.send(error);
+      console.log(error);
+      response.status(500).send(error);
     });
 };
